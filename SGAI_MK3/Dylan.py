058ca6f4da45bc6aa01484140e555e311cf5628d
Dylan Isaac <dylanisaac2005@gmail.com>
2022-07-25 18:51:07 -0400

    Updates
    Added Cell and renderConstants file

--- SGAI_MK3/Dylan.py
+++ SGAI_MK3/Dylan.py
@@ -3,35 +3,42 @@ import pygame
 from Int2 import Int2
 import time
 import constants
-start = time. process_time()
+import renderConstants
+from Cell import Cells
+start = time.process_time()
 pygame.init()
-size = Int2(700, 700)
-display_surface = pygame.display.set_mode((size.x, size.y))
+display_surface = pygame.display.set_mode((renderConstants.SIZE, renderConstants.SIZE))
 pygame.display.set_caption("Sus")
-
-day = pygame.transform.scale(pygame.image.load(r'Assets\\UI\\Backgrounds\\SunBackground.png'), (size.x, size.y))
-noon = pygame.transform.scale(pygame.image.load(r'Assets\\UI\\Backgrounds\\SunDownBackground.png'), (size.x, size.y))
-night = pygame.transform.scale(pygame.image.load(r'Assets\\UI\\Backgrounds\\MoonBackground.png'), (size.x, size.y))
-dayProgressBarHeight = size.y * 0.06
+#######
+day = pygame.transform.scale(pygame.image.load(r'Assets\\UI\\Backgrounds\\SunBackground.png'), (renderConstants.SIZE, renderConstants.SIZE))
+noon = pygame.transform.scale(pygame.image.load(r'Assets\\UI\\Backgrounds\\SunDownBackground.png'), (renderConstants.SIZE, renderConstants.SIZE))
+night = pygame.transform.scale(pygame.image.load(r'Assets\\UI\\Backgrounds\\MoonBackground.png'), (renderConstants.SIZE, renderConstants.SIZE))
+dayProgressBarHeight = renderConstants.SIZE * 0.06
 dayProgress = pygame.image.load(r'Assets\\UI\\DayProgressBar.png')
 dayProgress = pygame.transform.scale(dayProgress, (dayProgress.get_width() / dayProgress.get_height() * dayProgressBarHeight, dayProgressBarHeight))
-dayProgressPos = (size.x * (1 - 0.13) - dayProgress.get_width(), size.y * (1 - 0.005) - dayProgress.get_height())
+dayProgressPos = (renderConstants.SIZE * (1 - 0.13) - dayProgress.get_width(), renderConstants.SIZE * (1 - 0.005) - dayProgress.get_height())
 dayProgressBorderSize = 0.13
 dayProgressBorderSize = (dayProgressBorderSize * dayProgress.get_height() / dayProgress.get_width(), dayProgressBorderSize)
 dayProgressRectWidth = 3
 dayProgressRectBounds = dayProgressBorderSize[0] * dayProgress.get_width()
 dayProgressRectBounds = (dayProgressPos[0] + dayProgressRectBounds + 1, dayProgressPos[0] + dayProgress.get_width() - dayProgressRectBounds - dayProgressRectWidth + 1)
 dayProgressRect = pygame.Rect(dayProgressRectBounds[1], dayProgressPos[1] + dayProgress.get_height() * dayProgressBorderSize[1] + 0.5, dayProgressRectWidth, dayProgress.get_height() * (1 - dayProgressBorderSize[1] * 2) + 0.5)
+#######
+#######
 turn = 0
-cycleLen = 10
-noonLength = 2
 mainLoop = True
 while mainLoop:
     turn = int((time. process_time() - start))
-    display_surface.fill(constants.WHITE)
+    display_surface.fill((0, 0, 0))
     #######
-    if(turn % cycleLen < cycleLen/2):
-        if(turn % cycleLen > cycleLen/2 - 1 - noonLength):
+    for x in range(constants.ROWS):
+        cellX = int(renderConstants.GRIDRECT.left + constants.LINE_WIDTH + (constants.LINE_WIDTH + renderConstants.CELLSIZE) * x + renderConstants.CELLOFF)
+        for y in range(constants.ROWS):
+            cellY = int(renderConstants.GRIDRECT.top + constants.LINE_WIDTH + (constants.LINE_WIDTH + renderConstants.CELLSIZE) * y + renderConstants.CELLOFF)
+            display_surface.blit(Cells.grass.value.image, (cellX, cellY))
+    #######
+    if(turn % renderConstants.CYCLELEN < renderConstants.CYCLELEN/2):
+        if(turn % renderConstants.CYCLELEN > renderConstants.CYCLELEN/2 - 1 - renderConstants.NOONLENGTH):
             display_surface.blit(noon, (0, 0))
         else:
             display_surface.blit(day, (0, 0))
@@ -39,7 +46,7 @@ while mainLoop:
         display_surface.blit(night, (0, 0))
     #######
     display_surface.blit(dayProgress, dayProgressPos)
-    ratio = (turn % (cycleLen)) / (cycleLen - 1)
+    ratio = (turn % (renderConstants.CYCLELEN)) / (renderConstants.CYCLELEN - 1)
     dayProgressRect.left = dayProgressRectBounds[0] * (1 - ratio) + dayProgressRectBounds[1] * ratio
     pygame.draw.rect(display_surface, (255, 255, 255), dayProgressRect)
     #######
