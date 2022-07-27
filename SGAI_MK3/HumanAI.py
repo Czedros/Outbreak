25e4588a9beadb1ede1cc7348c5757de148eab66
plumshum <hannahyunzizhou#@gmail.com>
2022-07-27 15:27:10 -0400

    MCTS

--- SGAI_MK3/HumanAI.py
+++ SGAI_MK3/HumanAI.py
@@ -1,3 +1,12 @@
+##HumanAI.py 
+# Defintion:
+# Monte-Carlo Tree Search is a planning algorithm that accumulates value estimates obtained from Monte Carlo simulations in order to successively direct
+# simulations towards more highly-rewarded trajectories. We execute MCTS after encountering each new state to select an agent's action for that state: it is executed again to select the action for the next state. Each execution is an iterative process that simulates many trajectories starting from the current state to the terminal state. The core idea is to successively focus 
+# multiple simulations starting at the current state by extending the initial portions of trajectories that have received high evaluations 
+# from earlier simulations
+
+#selection, expansion, simulation, back propogation 
+
 import random as rd
 class Engine():
     role = ""
@@ -76,3 +85,51 @@ class Engine():
         idx = self.ACTION_SPACE.index(action_info[0])
         self.QTable[action_info[1]][idx] = self.QTable[action_info[1]][idx] + self.alpha * (action_info[3] + self.gamma * action_info[4]) -  self.QTable[action_info[1]][idx]
         # GameBoard.QTable[i] = GameBoard.QTable[i] + alpha * (reward[0] + gamma * NS) - GameBoard.QTable[i]
+
+
+# Our tree is each movement our human AI will make
+# each node represents the human's position 
+
+
+# main function for the Monte Carlo Tree Search
+#the starting parameter is the human's first position? root = human first position
+def monte_carlo_tree_search(root):
+     
+    while resources_left(time, computational power):
+        #while it is your turn  
+        leaf = traverse(root)
+        simulation_result = rollout(leaf)
+        backpropagate(leaf, simulation_result)
+         
+    return best_child(root)
+ 
+# function for node traversal
+def traverse(node):
+    while fully_expanded(node):
+        node = best_uct(node) #find the node that gives the best reward
+         
+    # in case no children are present (at the bottom) / node is terminal
+    return pick_unvisited(node.children) or node
+ 
+# function for the result of the simulation
+def rollout(node):
+    while non_terminal(node):
+        node = rollout_policy(node)
+    return result(node)
+ 
+# function for randomly selecting a child node
+def rollout_policy(node):
+    return pick_random(node.children)
+ 
+# function for backpropagation
+def backpropagate(node, result):
+    if is_root(node) return
+    node.stats = update_stats(node, result)
+    backpropagate(node.parent)
+ 
+# function for selecting the best child
+# node with highest number of visits
+def best_child(node):
+    pick child with highest number of visits
+
+#
